d62e29ea63116ac6f0600f340700d0177e0f7aa6
Ducdev710 <ducvuminh0710@gmail.com>
2024-04-07 15:58:22 +0700

    Chua load dc anh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,9 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
 }
